87e0382f58289f739a4df39a8b5cdd392535038e
gahdloot <davidlearn27@gmail.com>
2022-11-17 00:59:42 +0100

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn live_giver.live_giver.wsgi:application --log-file -
--- live_giver/live_giver/settings.py
+++ live_giver/live_giver/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-8+i21nd9(*(6he05$=f@%lf9453bmh6@9b(5-wdn76%gf-g^*t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,3 +133,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
