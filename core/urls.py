baf3f39265c6f19b0c482251741a32708bc66fb2
gahdloot <davidlearn27@gmail.com>
2022-11-18 08:50:02 +0100

    updating the sign-up page

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sign_up, name='sign_up'),
+    path('', views.home),
+    path('sign-up/', views.sign_up, name='sign_up'),
+    path('login/', views.login, name='login'),
     path('get_patients/', views.get_patients, name='get_patients'),
     path('set_donation_status/', views.set_donation_status, name='set_donation_status'),
     path('set_patient_status/', views.set_patient_status, name='set_patient_status'),
--- core/views.py
+++ core/views.py
@@ -7,14 +7,21 @@ from .verification import Verify
 
 @api_view(['POST'])
 def sign_up(request):
-    user = request.data
-    user.bvn = 54651333604
 
-    if Verify().bvn_verification(number=user.bvn):
+    #user = request.data(mutable=True)
+    user = request.data.copy()
+    user.update({'bvn':54651333604})
+    user.update({'age':29})
+    user.update({'weight':140})
+    user['bvn'] = 54651333604
+
+    if Verify().bvn_verification(number=user['bvn']):
         # client = Client(name=user.name, phone_number=user.phone_number, email=user.email, bvn=user.bvn, password=user.password, location=user.location, age=user.age, weight=user.weight, blood_group=user.blood_group)
         # client.save()
-        serializer = ClientCreationSerializer(data=request.data)
-        if serializer.is_valid():
+        user['bvn'] = 2147483647
+        serializer = ClientCreationSerializer(data=user)
+        print(type(user['weight']))
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response({'message':'created'}, status=status.HTTP_201_CREATED)
         return Response({'message':'something is wrong with the data'})
@@ -24,12 +31,12 @@ def sign_up(request):
 @api_view(['POST'])
 def login(request):
     
-    if request.data.email:
+    if request.data['email']:
         try:
-            user = Client.objects.get(email=request.data.email)
+            user = Client.objects.get(email=request.data['email'])
         except Client.DoesNotExist:
             return Response({'message':'This User doesnot exist, please check email'}, status=status.HTTP_404_NOT_FOUND)
-        if request.data.password == user.password:
+        if request.data['password'] == user.password:
             return Response({'message':'successfully logged in'}, status=status.HTTP_202_ACCEPTED)
         return Response({'message': 'password does not match'}, status=status.HTTP_406_NOT_ACCEPTABLE)
     return Response({'message': 'email field is blank'}, status=status.HTTP_400_BAD_REQUEST)
@@ -38,7 +45,7 @@ def login(request):
 @api_view(['GET'])
 def get_patients(request):
 
-    if request.data.blood_group == 'O-':
+    if request.data['blood_group'] == 'O-':
         patient_filter = Client.objects.filter(needs_donation=True).all()
         serializer = ClientSerializer(patient_filter, many=True)
         return Response(serializer)
@@ -57,8 +64,8 @@ def get_all_clients(request):
 
 @api_view(['POST'])
 def set_donation_status(request):
-    client_info = dict(request.data)
-    client = Client.objects.get(id=client_info.id)
+    client_info = request.data
+    client = Client.objects.get(id=client_info['id'])
     if client.eligible_donate():
         client.wants_to_donate= client_info.wants_to_donate
         client.save()
